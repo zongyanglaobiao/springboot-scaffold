34b953859d32f86b5495dc16b8dae7ce7b4d5773
xxl <3578144921@qq.com>
2024-05-18 11:03:02 +0800

    feat(Application)一出无用的包

--- application/src/main/java/com/xxl/scaffold/Application.java
+++ application/src/main/java/com/xxl/scaffold/Application.java
@@ -1,19 +1,14 @@
 package com.xxl.scaffold;
 
-import com.xxl.sdk.log.AsyncLogger;
-import com.xxl.sdk.util.thead.TheadUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Indexed;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @author: anyone
