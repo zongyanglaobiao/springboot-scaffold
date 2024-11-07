b65411d3d5929da558353033bd47aa2b0b5bf1df
xxl <3578144921@qq.com>
2024-11-07 16:11:03 +0800

    fix(线程工具类): 修改线程工具类

--- sdk/src/main/java/com/aks/sdk/util/thread/ThreadUtils.java
+++ sdk/src/main/java/com/aks/sdk/util/thread/ThreadUtils.java
@@ -11,6 +11,8 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class ThreadUtils {
 
+    private static final String DEFAULT_THREAD_NAME = "default--thread--";
+
     public static ThreadPoolExecutor createThreadPool() {
         // 根据您的服务器的CPU核心数来动态设置核心线程数，这里假设是CPU核心数的两倍
         int corePoolSize = Runtime.getRuntime().availableProcessors() * 2;
@@ -22,12 +24,26 @@ public class ThreadUtils {
         int keepAliveTime = 60;
         //拒绝策略,人数过多就报错
         ThreadPoolExecutor.AbortPolicy abortPolicy = new ThreadPoolExecutor.AbortPolicy();
-        return createThreadPool(corePoolSize,maximumPoolSize,keepAliveTime,seconds,abortPolicy);
+        return createThreadPool(corePoolSize,maximumPoolSize,keepAliveTime,seconds,abortPolicy,DEFAULT_THREAD_NAME);
     }
 
-    public static ThreadPoolExecutor createThreadPool(int corePoolSize, int maximumPoolSize, int keepAliveTime, TimeUnit unit, RejectedExecutionHandler handler) {
+    public static ThreadPoolExecutor createThreadPool(String threadName) {
+        // 根据您的服务器的CPU核心数来动态设置核心线程数，这里假设是CPU核心数的两倍
+        int corePoolSize = Runtime.getRuntime().availableProcessors() * 2;
+        //最大创建线程数
+        int maximumPoolSize = corePoolSize * 2;
+        //时间单位
+        TimeUnit seconds = TimeUnit.SECONDS;
+        //存活时间
+        int keepAliveTime = 60;
+        //拒绝策略,人数过多就报错
+        ThreadPoolExecutor.AbortPolicy abortPolicy = new ThreadPoolExecutor.AbortPolicy();
+        return createThreadPool(corePoolSize,maximumPoolSize,keepAliveTime,seconds,abortPolicy,threadName);
+    }
+
+    public static ThreadPoolExecutor createThreadPool(int corePoolSize, int maximumPoolSize, int keepAliveTime, TimeUnit unit, RejectedExecutionHandler handler, String threadName) {
         //线程工厂
-        ChatThreadFactory factory = new ChatThreadFactory();
+        DefaultThreadFactory factory = new DefaultThreadFactory(threadName);
         return new ThreadPoolExecutor(corePoolSize,
                 maximumPoolSize,
                 keepAliveTime,
@@ -37,11 +53,18 @@ public class ThreadUtils {
                 handler);
     }
 
-    static class ChatThreadFactory implements ThreadFactory {
+    static class DefaultThreadFactory implements ThreadFactory {
+
+        public final String threadName;
+
+        public DefaultThreadFactory(String threadName) {
+            this.threadName = threadName;
+        }
+
         private static final AtomicInteger COUNT = new AtomicInteger(0);
         @Override
         public Thread newThread(Runnable r) {
-            return new Thread(r,"chat--thread--" + COUNT.incrementAndGet());
+            return new Thread(r,threadName + COUNT.incrementAndGet());
         }
     }
 }
