d60200e64f90f5d412f195709f014b419c6be094
xxl <3578144921@qq.com>
2024-11-11 10:06:06 +0800

    perf(线程池工具类): 优化线程工具类

--- sdk/src/main/java/com/aks/sdk/util/thread/ThreadUtils.java
+++ sdk/src/main/java/com/aks/sdk/util/thread/ThreadUtils.java
@@ -11,34 +11,70 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class ThreadUtils {
 
-    private static final String DEFAULT_THREAD_NAME = "default--thread--";
+    public static final String DEFAULT_THREAD_NAME;
+
+    public static final int DEFAULT_THREAD_POOL_SIZE;
+
+    public static final int DEFAULT_MAX_THREAD_POOL_SIZE;
+
+    public static final int DEFAULT_KEEP_ALIVE_TIME;
+
+    public static final TimeUnit DEFAULT_TIME_UNIT;
+
+    public static final BlockingQueue<Runnable> DEFAULT_QUEUE;
+
+    public static final RejectedExecutionHandler DEFAULT_REJECTED_EXECUTION_HANDLER;
+
+    static {
+        DEFAULT_THREAD_NAME = "default-thread--";
+        // 根据您的服务器的CPU核心数来动态设置核心线程数
+        DEFAULT_THREAD_POOL_SIZE = Runtime.getRuntime().availableProcessors() * 2;
+        //最大创建线程数
+        DEFAULT_MAX_THREAD_POOL_SIZE = DEFAULT_THREAD_POOL_SIZE * 2;
+        //存活时间
+        DEFAULT_KEEP_ALIVE_TIME = 60;
+        //时间单位
+        DEFAULT_TIME_UNIT = TimeUnit.SECONDS;
+        //任务队列
+        DEFAULT_QUEUE = new LinkedBlockingQueue<>();
+        //拒绝策略
+        DEFAULT_REJECTED_EXECUTION_HANDLER = new ThreadPoolExecutor.AbortPolicy();
+    }
+
 
     public static ThreadPoolExecutor createThreadPool() {
         return createThreadPool(DEFAULT_THREAD_NAME);
     }
 
     public static ThreadPoolExecutor createThreadPool(String threadName) {
-        // 根据您的服务器的CPU核心数来动态设置核心线程数，这里假设是CPU核心数的两倍
-        int corePoolSize = Runtime.getRuntime().availableProcessors() * 2;
-        //最大创建线程数
-        int maximumPoolSize = corePoolSize * 2;
-        //时间单位
-        TimeUnit seconds = TimeUnit.SECONDS;
-        //存活时间
-        int keepAliveTime = 60;
-        //拒绝策略,人数过多就报错
-        ThreadPoolExecutor.AbortPolicy abortPolicy = new ThreadPoolExecutor.AbortPolicy();
-        return createThreadPool(corePoolSize,maximumPoolSize,keepAliveTime,seconds,abortPolicy,threadName);
+        return createThreadPool(threadName,DEFAULT_QUEUE);
     }
 
-    public static ThreadPoolExecutor createThreadPool(int corePoolSize, int maximumPoolSize, int keepAliveTime, TimeUnit unit, RejectedExecutionHandler handler, String threadName) {
+    public static ThreadPoolExecutor createThreadPool(String threadName,BlockingQueue<Runnable> queue) {
+       return createThreadPool(
+               DEFAULT_THREAD_POOL_SIZE,
+               DEFAULT_MAX_THREAD_POOL_SIZE,
+               DEFAULT_KEEP_ALIVE_TIME,
+               DEFAULT_TIME_UNIT,
+               queue,
+               DEFAULT_REJECTED_EXECUTION_HANDLER,
+               threadName);
+    }
+
+    public static ThreadPoolExecutor createThreadPool(int corePoolSize,
+                                                      int maximumPoolSize,
+                                                      int keepAliveTime,
+                                                      TimeUnit unit,
+                                                      BlockingQueue<Runnable> queue,
+                                                      RejectedExecutionHandler handler,
+                                                      String threadName) {
         //线程工厂
         DefaultThreadFactory factory = new DefaultThreadFactory(threadName);
         return new ThreadPoolExecutor(corePoolSize,
                 maximumPoolSize,
                 keepAliveTime,
                 unit,
-                new LinkedBlockingQueue<>(),
+                queue,
                 factory,
                 handler);
     }
